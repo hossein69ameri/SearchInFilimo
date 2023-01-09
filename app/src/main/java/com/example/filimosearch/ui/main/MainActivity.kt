e1745e1c3d8ed9693dcfe8435b210e92cbcdb8bc
Hossein <hossein.arabameri69@gmail.com>
2023-01-09 10:50:17 +0330

    Add Library To Gradle and Create Packages

---
+++ app/src/main/java/com/example/filimosearch/ui/main/MainActivity.kt
@@ -0,0 +1,12 @@
+package com.example.filimosearch.ui.main
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.example.filimosearch.R
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
